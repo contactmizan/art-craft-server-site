4f7f5131dffa98d7fe3668250941a890ab68140c
contactmizan <contactmizan1206@gmail.com>
2024-12-08 02:46:38 +0600

    add .env file and git ignore

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -8,13 +9,11 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-//artCraft
-//6TgAD1y2B6RBI1dO
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
-
-
-// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.9czch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://artCraft:6TgAD1y2B6RBI1dO@cluster0.9czch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9czch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
